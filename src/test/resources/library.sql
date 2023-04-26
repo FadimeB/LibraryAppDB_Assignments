4939459173e047df36e12e33973381b191a2a3ed
FadimeB <kafkaskizi@hotmail.com>
2023-04-26 16:33:33 -0400

    completed

--- src/test/resources/library.sql
+++ src/test/resources/library.sql
@@ -18,3 +18,20 @@ select * from users;
 -- US 03
 select count(*) from book_borrow
 where is_returned=0;
+
+-- US 7
+select full_name,b.name,bb.borrowed_date from users u
+inner join  book_borrow bb on u.id = bb.user_id
+inner join books b on bb.book_id = b.id
+where b.name='Self Confidence'
+
+order by 3 desc;
+
+select distinct name from books
+where name='Self Confidence' ;
+
+SELECT * from books;
+
+select * from book_borrow;
+
+
